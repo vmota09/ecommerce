2dd175b7f90f8edd42b791991590d185ef0e7d4c
vmota09 <vmota0926@gmail.com>
2025-04-24 17:26:17 -0300

    Fez o autoload das classes

--- index.php
+++ index.php
@@ -2,16 +2,20 @@
 
 require_once("vendor/autoload.php");
 
+error_reporting(E_ALL & ~E_DEPRECATED & ~E_NOTICE); //some com os erros que dá pela versão antiga do slim
+
 $app = new \Slim\Slim();
 
 $app->config('debug', true);
 
 $app->get('/', function() {
     
-	echo "OK";
+
+    $sql = new Hcode\DB\Sql();
+    $results = $sql->select("SELECT * FROM tb_users");
+
+    echo json_encode($results);
 
 });
 
 $app->run();
-
- ?>
--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -6,8 +6,18 @@ namespace Composer\Autoload;
 
 class ComposerStaticInit63de4cbced032f8d0a1c270f2c74b2fe
 {
-    public static $fallbackDirsPsr4 = array (
-        0 => __DIR__ . '/..' . '/hcodebr/php-classes/src',
+    public static $prefixLengthsPsr4 = array (
+        'H' => 
+        array (
+            'Hcode\\' => 6,
+        ),
+    );
+
+    public static $prefixDirsPsr4 = array (
+        'Hcode\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/hcodebr/php-classes/src',
+        ),
     );
 
     public static $prefixesPsr0 = array (
@@ -42,7 +52,8 @@ class ComposerStaticInit63de4cbced032f8d0a1c270f2c74b2fe
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
-            $loader->fallbackDirsPsr4 = ComposerStaticInit63de4cbced032f8d0a1c270f2c74b2fe::$fallbackDirsPsr4;
+            $loader->prefixLengthsPsr4 = ComposerStaticInit63de4cbced032f8d0a1c270f2c74b2fe::$prefixLengthsPsr4;
+            $loader->prefixDirsPsr4 = ComposerStaticInit63de4cbced032f8d0a1c270f2c74b2fe::$prefixDirsPsr4;
             $loader->prefixesPsr0 = ComposerStaticInit63de4cbced032f8d0a1c270f2c74b2fe::$prefixesPsr0;
             $loader->classMap = ComposerStaticInit63de4cbced032f8d0a1c270f2c74b2fe::$classMap;
 
