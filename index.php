477566bd0c24c316aeca81473394bdcd233e3771
vmota09 <vmota0926@gmail.com>
2025-04-25 14:50:51 -0300

    Criando a classe Page

--- index.php
+++ index.php
@@ -1,7 +1,6 @@
-<?php 
+<?php
 
 require_once("vendor/autoload.php");
-
 error_reporting(E_ALL & ~E_DEPRECATED & ~E_NOTICE); //some com os erros que dá pela versão antiga do slim
 
 $app = new \Slim\Slim();
@@ -9,13 +8,15 @@ $app = new \Slim\Slim();
 $app->config('debug', true);
 
 $app->get('/', function() {
-    
 
-    $sql = new Hcode\DB\Sql();
-    $results = $sql->select("SELECT * FROM tb_users");
+    $page = new Hcode\Page();
 
-    echo json_encode($results);
+    $page->setTpl("index");
 
 });
 
 $app->run();
+
+?>
+
+
